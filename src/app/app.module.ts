d251d3dc210a22f9d27fb8f161b486f7f07a2c41
GS Vinay <vinay.g_s@siemens-healthineers.com>
2025-01-28 23:20:12 +0530

    Updated app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgChartsModule } from 'ng2-charts';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
